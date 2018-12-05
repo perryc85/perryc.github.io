21d69a9009cd9a0e3d1909adff50e9229ff47007
cjperry85@outlook.com <perryc85>
2018-12-05 23:49:07 +0000

    colab -> github

--- perryc85/index.js
+++ perryc85/index.js
@@ -7,7 +7,17 @@ const HOSTED_URLS = {
 };
 
 const examples = {
-    'Sentence_1' : 'sentences'
+    'sample_sentence_1': 'This to be Love, that your spirit to live in a natural holiness with
+the Beloved, and your bodies to be a sweet and natural delight that
+shall be never lost of a lovely mystery.'
+
+    'sample_sentence_2' : 'This to be Love, that your spirit to live in a natural holiness with
+the Beloved, and your bodies to be a sweet and natural delight that
+shall be never lost of a lovely mystery!'
+
+    'sample_sentence_3' : 'Being an account of their Adventures in the Strange places of the Earth,
+after the foundering of the good ship _Glen Carrig_ through striking upon
+a hidden rock in the unknown seas to the Southward.'
 };
 
 function status(statusText) {
